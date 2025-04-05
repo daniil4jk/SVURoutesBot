3ab76e9f620a62989230080735e677d6a50efef2
daniil4jk <daniilprotop@bk.ru>
2025-04-05 22:11:37 +0500

    Исправлен баг

--- src/main/java/ru/daniil4jk/svuroutes/tgbot/web/StatusController.java
+++ src/main/java/ru/daniil4jk/svuroutes/tgbot/web/StatusController.java
@@ -2,15 +2,10 @@ package ru.daniil4jk.svuroutes.tgbot.web;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.PrintWriter;
-import java.io.StringWriter;
 
 
 @Setter
@@ -18,7 +13,7 @@ import java.io.StringWriter;
 @RestController
 @RequestMapping("/status")
 public class StatusController {
-    private Exception lastThrowable = null;
+    private Throwable lastThrowable = null;
 
     @GetMapping
     public ResponseEntity<String> getStatus() {
