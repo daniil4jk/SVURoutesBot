fda0ceb51c6e29a2ee16a7086d3f94e3858bd6b2
daniil4jk <daniilprotop@bk.ru>
2025-04-05 22:02:47 +0500

    Исправлена инкапсуляция

--- src/main/java/ru/daniil4jk/svuroutes/tgbot/command/admin/CreateEventCmd.java
+++ src/main/java/ru/daniil4jk/svuroutes/tgbot/command/admin/CreateEventCmd.java
@@ -11,6 +11,7 @@ import org.telegram.telegrambots.meta.bots.AbsSender;
 import ru.daniil4jk.svuroutes.tgbot.bot.simpleexecuter.SimpleExecuter;
 import ru.daniil4jk.svuroutes.tgbot.command.assets.ProtectedBotCommand;
 import ru.daniil4jk.svuroutes.tgbot.content.DTO.Route;
+import ru.daniil4jk.svuroutes.tgbot.db.entity.EventEntity;
 import ru.daniil4jk.svuroutes.tgbot.db.service.assets.EventService;
 import ru.daniil4jk.svuroutes.tgbot.expected.ExpectedEvent;
 import ru.daniil4jk.svuroutes.tgbot.keyboard.inline.BooleanInlineKeyboard;
@@ -71,105 +72,110 @@ public class CreateEventCmd extends ProtectedBotCommand {
             log.error(e.getLocalizedMessage(), e);
         }
     }
-}
 
-class GetSomethingEvent extends ExpectedEvent<Message> {
 
-    public GetSomethingEvent(AtomicLong chatId, Consumer<String> setter,
+    private static class GetSomethingEvent extends ExpectedEvent<Message> {
+
+        public GetSomethingEvent(AtomicLong chatId, Consumer<String> setter,
+                                 String notification, Runnable addNext) {
+            super(m -> {
+                if (m.getChatId() != chatId.get()) chatId.set(m.getChatId());
+                setter.accept(m.getText());
+                addNext.run();
+            });
+            var getDateMessage = SendMessage.builder().text(notification).chatId(chatId.get()).build();
+            firstNotification(getDateMessage);
+            notification(getDateMessage);
+            onException(e -> SendMessage.builder()
+                    .text(e.getLocalizedMessage()).chatId(chatId.get()).build());
+            removeOnException(false);
+        }
+    }
+
+    private static class GetNumberEvent extends GetSomethingEvent {
+        public GetNumberEvent(AtomicLong chatId, int calendarConst, Calendar c,
+                              int maxValue, String notification, Runnable addNext) {
+            super(chatId, t -> {
+                try {
+                    int value = Integer.parseInt(t);
+                    if (value < 0) throw new IllegalArgumentException(
+                            "Значение этого параметра не может быть меньше 0");
+                    if (value > maxValue) throw new IllegalArgumentException(
+                            "Значение этого параметра не должно быть больше " + maxValue);
+                    c.set(calendarConst, value);
+                } catch (NumberFormatException e) {
+                    throw new NumberFormatException("Введите только число, без лишних букв и символов");
+                }
+            }, notification, addNext);
+        }
+    }
+
+    private static class GetMonthInput extends GetSomethingEvent {
+        private static final String[] monthNames =
+                new String[]{"январь", "февраль", "март",
+                        "апрель", "май", "июнь",
+                        "июль", "август", "сентябрь",
+                        "октябрь", "ноябрь", "декабрь"};
+
+        public GetMonthInput(AtomicLong chatId, Calendar c,
                              String notification, Runnable addNext) {
-        super(m -> {
-            if (m.getChatId() != chatId.get()) chatId.set(m.getChatId());
-            setter.accept(m.getText());
-            addNext.run();
-        });
-        var getDateMessage = SendMessage.builder().text(notification).chatId(chatId.get()).build();
-        firstNotification(getDateMessage);
-        notification(getDateMessage);
-        onException(e -> SendMessage.builder()
-                .text(e.getLocalizedMessage()).chatId(chatId.get()).build());
-        removeOnException(false);
+            super(chatId, s -> {
+                int monthNumber = ArrayUtils.indexOf(monthNames, s.toLowerCase());
+                if (monthNumber == -1) throw new IllegalArgumentException(
+                        "Введите название месяца, например \"октябрь\"");
+                c.set(Calendar.MONTH, monthNumber);
+            }, notification, addNext);
+        }
     }
-}
 
-class GetNumberEvent extends GetSomethingEvent {
-    public GetNumberEvent(AtomicLong chatId, int calendarConst, Calendar c,
-                          int maxValue, String notification, Runnable addNext) {
-        super(chatId, t -> {
-            try {
-                int value = Integer.parseInt(t);
-                if (value < 0) throw new IllegalArgumentException(
-                        "Значение этого параметра не может быть меньше 0");
-                if (value > maxValue) throw new IllegalArgumentException(
-                        "Значение этого параметра не должно быть больше " + maxValue);
-                c.set(calendarConst, value);
-            } catch (NumberFormatException e) {
-                throw new NumberFormatException("Введите только число, без лишних букв и символов");
-            }
-        }, notification, addNext);
-    }
-}
+    private static class ConfirmEvent extends ExpectedEvent<CallbackQuery> {
+        private static final String[] monthNames =
+                new String[]{"январе", "феврале", "марте",
+                        "апреле", "мае", "июне",
+                        "июле", "августе", "сентябре",
+                        "октябре", "ноябре", "декабре"};
 
-class GetMonthInput extends GetSomethingEvent {
-    private static final String[] monthNames =
-            new String[]{"январь", "февраль", "март",
-                         "апрель", "май", "июнь",
-                         "июль", "август", "сентябрь",
-                         "октябрь", "ноябрь", "декабрь"};
+        public ConfirmEvent(AtomicLong chatId, SimpleExecuter executer,
+                            EventService service, Route route, Calendar calendar,
+                            EventsKeyboard keyboardToUpdate) {
 
-    public GetMonthInput(AtomicLong chatId, Calendar c,
-                         String notification, Runnable addNext) {
-        super(chatId, s -> {
-            int monthNumber = ArrayUtils.indexOf(monthNames, s.toLowerCase());
-            if (monthNumber == -1) throw new IllegalArgumentException(
-                    "Введите название месяца, например \"октябрь\"");
-            c.set(Calendar.MONTH, monthNumber);
-        }, notification, addNext);
-    }
-}
+            super(q -> {
+                if (String.valueOf(true).equals(q.getData())) {
 
-class ConfirmEvent extends ExpectedEvent<CallbackQuery> {
-    private static final String[] monthNames =
-            new String[]{"январе", "феврале", "марте",
-                    "апреле", "мае", "июне",
-                    "июле", "августе", "сентябре",
-                    "октябре", "ноябре", "декабре"};
+                    EventEntity newEntity = service.createNew(route.getId(), calendar.getTime(),
+                            null, 1);
+                    executer.sendSimpleTextMessage(
+                            "Событие успешно добавлено", chatId.get());
+                    keyboardToUpdate.add(newEntity);
 
-    public ConfirmEvent(AtomicLong chatId, SimpleExecuter executer,
-                        EventService service, Route route, Calendar calendar,
-                        EventsKeyboard keyboardToUpdate) {
-        super(q -> {
-            if (String.valueOf(true).equals(q.getData())) {
-                var newEntity = service.createNew(route.getId(), calendar.getTime());
-                executer.sendSimpleTextMessage(
-                        "Событие успешно добавлено", chatId.get());
-                keyboardToUpdate.add(newEntity);
-            } else if (String.valueOf(false).equals(q.getData())) {
-                executer.sendSimpleTextMessage(
-                        "Добавление события успешно отменено", chatId.get());
-            } else {
-                throw new IllegalArgumentException("Выберите \"Все так\" или \"отменить\"");
-            }
-        });
+                } else if (String.valueOf(false).equals(q.getData())) {
+                    executer.sendSimpleTextMessage(
+                            "Добавление события успешно отменено", chatId.get());
+                } else {
+                    throw new IllegalArgumentException("Выберите \"Все так\" или \"отменить\"");
+                }
+            });
 
-        var notification = SendMessage.builder()
-                .text(String.format("""
-                        Вы хотите создать событие с:
-                        Маршрутом - %s
-                        В %s
-                        %d числа
-                        В %d часов, %d минут
-                        """, route.getName(),
-                        monthNames[calendar.get(Calendar.MONTH)],
-                        calendar.get(Calendar.DAY_OF_MONTH),
-                        calendar.get(Calendar.HOUR_OF_DAY),
-                        calendar.get(Calendar.MINUTE)))
-                .chatId(chatId.get())
-                .replyMarkup(new BooleanInlineKeyboard("Все так", "Отменить"))
-                .build();
-        firstNotification(notification);
-        notification(notification);
-        onException(e -> SendMessage.builder()
-                .text(e.getLocalizedMessage()).chatId(chatId.get()).build());
-        removeOnException(false);
+            var notification = SendMessage.builder()
+                    .text(String.format("""
+                                    Вы хотите создать событие с:
+                                    Маршрутом - %s
+                                    В %s
+                                    %d числа
+                                    В %d часов, %d минут
+                                    """, route.getName(),
+                            monthNames[calendar.get(Calendar.MONTH)],
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            calendar.get(Calendar.HOUR_OF_DAY),
+                            calendar.get(Calendar.MINUTE)))
+                    .chatId(chatId.get())
+                    .replyMarkup(new BooleanInlineKeyboard("Все так", "Отменить"))
+                    .build();
+            firstNotification(notification);
+            notification(notification);
+            onException(e -> SendMessage.builder()
+                    .text(e.getLocalizedMessage()).chatId(chatId.get()).build());
+            removeOnException(false);
+        }
     }
 }
