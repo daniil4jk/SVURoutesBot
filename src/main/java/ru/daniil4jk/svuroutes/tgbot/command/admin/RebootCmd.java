def24480d2bf229d554ad2a48df5bffd6aea3197
daniil4jk <daniilprotop@bk.ru>
2025-04-11 18:17:26 +0500

    Добавлена команда для перезагрузки

---
+++ src/main/java/ru/daniil4jk/svuroutes/tgbot/command/admin/RebootCmd.java
@@ -0,0 +1,50 @@
+package ru.daniil4jk.svuroutes.tgbot.command.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+import ru.daniil4jk.svuroutes.tgbot.bot.simpleexecuter.SimpleExecuter;
+import ru.daniil4jk.svuroutes.tgbot.command.assets.ProtectedBotCommand;
+import ru.daniil4jk.svuroutes.tgbot.db.service.assets.UserService;
+
+@Slf4j
+@Component
+public class RebootCmd extends ProtectedBotCommand {
+    @Autowired
+    private ConfigurableApplicationContext context;
+    @Autowired
+    private UserService userService;
+
+    public RebootCmd() {
+        super("reboot", "reboot application");
+    }
+
+    /**
+     * Construct a command
+     *
+     * @param commandIdentifier the unique identifier of this command (e.g. the command string to
+     *                          enter into chat)
+     * @param description       the description of this command
+     */
+    public RebootCmd(String commandIdentifier, String description) {
+        super(commandIdentifier, description);
+    }
+
+    @Override
+    protected void protectedExecute(AbsSender absSender, long chatId, String[] strings) {
+        ((SimpleExecuter) absSender).nonExceptionExecute(SendMessage.builder()
+                .text("Выполняю остановку бота...")
+                .chatId(chatId)
+                .build());
+        try {
+            log.info("Остановка бота вызвана пользователем {}", userService.get(chatId).getUsername());
+        } catch (Exception e) {
+            log.info("Остановка бота вызвана пользователем с chatId: {}", chatId);
+        }
+        SpringApplication.exit(context);
+    }
+}
